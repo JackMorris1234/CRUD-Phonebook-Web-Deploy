8567b6cf77869ba0c36276076ff67ddc6beba2f7
JackMorris1234 <163089764+JackMorris1234@users.noreply.github.com>
2026-09-22 13:42:12 -0400

    Added Dist from frontend build

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ morgan.token(`details`,function(req,res){
 })
 const cors=require('cors')
 const app = express()
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length]- :response-time ms :details'))
 app.use(cors())
