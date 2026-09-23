76ed7143465fc5eef1137106da411a3ff38c32f0
JackMorris1234 <163089764+JackMorris1234@users.noreply.github.com>
2026-09-23 15:38:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-require('dotenv').config
+require('dotenv').config()
 const morgan=require('morgan')
 const cors=require('cors')
 const Entry=require('./models/entry')
